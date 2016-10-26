799e25987dd57dc3762ea3d936c044fbed18a3f8
Dylan <revanistt3@protonmail.com>
2016-10-26 16:55:25 -0400

    fixed "editable" type labels and properties

--- src/editable.jsx
+++ src/editable.jsx
@@ -2,14 +2,14 @@
 import React from 'react'
 
 export default function<T>(props:{lastValue:T, edit:boolean, update:(x:T) => void, toggleEdit:() => void}) {
-  let SwitchButton = (sProps/*:{text:string}*/) => <button style={{color:'#000099'}}>{sProps.x}</button>
+  let SwitchButton = (sProps:{text:string}) => <button style={{color:'#000099'}}>{sProps.x}</button>
   return props.edit
     ? <p>
       {props.lastValue}
-      <SwitchButton x={'edit'}/>
+      <SwitchButton text={'edit'}/>
     </p>
     : <div>
       <input onChange={props.update} value={props.lastValue}/>
-      <SwitchButton x={'submit'}/>
+      <SwitchButton text={'submit'}/>
     </div>
 }
