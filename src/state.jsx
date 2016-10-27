02f5bd4fb374e6cbb0ea242547e58f59be553f9b
dmcrae17 <revanistt3@protonmail.com>
2016-10-27 10:21:03 -0400

    added saved property in store

--- src/state.jsx
+++ src/state.jsx
@@ -1,15 +1,36 @@
+// @flow
 import { combineReducers } from 'redux'
 import { reducer as user } from './profileManagement/view.jsx'
 
-function windowVars(state = {edit:true}, action: {type: string} = {type:''}) {
-  switch (action.type) {
-    case 'TOGGLE':
-      return {
-        edit: !state.edit
-      }
+function windowVars(state = {edit:true, saved:false}, action: {type: string, what?: string} = {type:'', what:''}) {
+    if (action.type ==='TOGGLE') {
+        switch (action.what) {
+            case undefined:
+                return {
+                    ...state,
+                    edit: !state.edit
+                }
+            case 'EDIT':
+                return {
+                    ...state,
+                    edit: !state.edit
+                }
+            case 'saved':
+                return {
+                    ...state,
+                    saved: !state.saved
+                }
+
+        }
+       } else {
+        return state
+    }
+  /*switch (action.type) {
+    case :
+      
     default:
       return state
-  }
+  }*/
 }
 
 export default combineReducers({
