c9ee2d3206f5011ac826de2a520db1400cb02dee
dmcrae17 <revanistt3@protonmail.com>
2016-11-02 10:01:56 -0400

    made contact reducer

--- src/contactList/contact/state.jsx
+++ src/contactList/contact/state.jsx
@@ -1,1 +1,19 @@
 ﻿// @flow
+import {Profile as Contact} from '../../profileManagement/profile.jsx'
+
+type State = {profile:Contact, messageBox:boolean, messageText:string}
+export default function(state:State | {} = {}, action) {
+    switch(action.type) {
+        case 'ADD_CONTACT':
+            return {
+                ...state,
+                profile:action.newContact,
+                messageBox: false,
+                messageText: ''
+            }
+        case 'REMOVE_CONTACT':
+            return {}
+        default:
+            return state
+    }
+}
