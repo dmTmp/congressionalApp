5e39747028cbdd733867e938b93bac90b9f790e0
Dylan <revanistt3@protonmail.com>
2016-11-13 21:12:24 -0500

    added more profile tests

--- src/profileManagement/test.jsx
+++ src/profileManagement/test.jsx
@@ -1,8 +1,21 @@
 import reducer, {ReplaceProfile} from './state.jsx'
 import assert from 'assert'
 import {User} from './profile.jsx'
-
+import profile, {replaceProfile, updateProfile, deleteProfile} from './state.jsx'
 
 export default () => {
-
+  describe('profile management', () => {
+    it('intialize contact', () => {
+      const person = new User('Person')
+      assert.equal(person, profile(undefined, replaceProfile(person)))
+    }),
+    describe('change name', () => {
+      const person = new User('Person')
+      const newName = 'new name'
+      const newPerson = profile(person, updateProfile(newName))
+      it('name', () => {assert.equal(newPerson.name, newName)})
+      it('rnd', () => {assert.equal(person.rnd, newPerson.rnd)})
+      it('picture', () => {assert.equal(person.picture, person.picture)})
+    })
+  })
 }
--- src/test.jsx
+++ src/test.jsx
@@ -1,15 +1,8 @@
-var assert = require('assert')
+import assert from 'assert'
 import contact from './contactList/contact/state.jsx'
 import {Profile as Contact, User} from './profileManagement/profile.jsx'
-import profile, {replaceProfile} from './profileManagement/state.jsx'
+import profileTest from './profileManagement/test.jsx'
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
 describe('contact reducer', function() {
   it('defualt', () => {
     assert.equal(contact(undefined, {}), null)
@@ -19,10 +12,4 @@ describe('contact reducer', function() {
     assert.equal(contact({}, {type:'CONTACT_ADD',newContact:c}).profile,c)
   })
 })
-describe('profile management', () => {
-
-  it('intialize contact', () => {
-    const person = new User('Person')
-    assert.equal(person, profile(undefined, replaceProfile(person)))
-  })
-})
+profileTest()
